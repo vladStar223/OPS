e88b0997d57b6c16207ceb430cc14c682b20ab12
vladStar223 <vlad32kolm@gmail.com>
2025-03-28 13:17:28 +1000

    ddd

--- OPS/OPS.cpp
+++ OPS/OPS.cpp
@@ -6,197 +6,203 @@
 #include<string>
 #include <set>
 using namespace std;
-int size_s = 0;
-struct Node
-{
+struct Node {
 	Node* next = nullptr;
-	string data = "2";
-	int size = 0;
-
+	string data;
 };
-void push(Node*& p_top, string x) {
-	Node* n = new Node;
-	n->data = x;
-	n->next = nullptr;
-	if (p_top == nullptr) {
-		p_top = new Node;
-		p_top->next = n;
-	}
-	else {
-		int s = p_top->size;
-		n->next = p_top->next;
-		n->size = p_top->size + 1;
-		p_top->next = n;
-	}
-	
 
-}
-string top(Node* p_top) {
-	if (p_top == nullptr) {
-		throw "Error";
-	}
-	return p_top->next->data;
-}
-void pop(Node*& p_top) {
-	if (p_top == nullptr) {
-		throw "Error";
-	}
-	else {
-		Node* t = p_top->next;
-		p_top->next = p_top->next->next;
-		delete t;
-	}
-}
-bool isEmpty(Node* p_top) {
-	return (p_top->next == nullptr);
-}
- int get_size(Node*  p_top) {
-	 if (p_top == nullptr) {
-		 return 0;
-	 }
-	 return p_top->size;
-}
+struct Stack {
+private:
 
-
-const set<string> operations = { "+","-","*","/","^","(",")" }; // операции доступные сейчас 
-const set<string> one_operations = { "sin","cos","tg","sqrt" }; // операции доступные сейчас 
-int get_priority(string x, int p = 0) {
-	if (x == "(" || x == ")") {
-		return 0;
+	Node* node = nullptr;
+	int size = 0;
+public:
+	static Stack* create() {
+		return new Stack();
 	}
-	else if (x == "+" || x == "-") {
-		return 1;
-	}
-	else if (x == "*" || x == "/") {
-		return 2;
-	}
-	else if (x == "^") {
-		return 3;
-	}
-	else if (x == "-" && p == 1) {
-		return 10;
-	}
-	else if (x == "sin" || x == "cos" || x == "tg" || x == "sqrt") {
-		return 5;
-	}
-}
-void operationsa_procces(Node* p_begin, string operation, bool one_opertion = false) {
-	if ((one_opertion == false && (get_size(p_begin) <= 1))) {
-		throw runtime_error("Error");
-	}
-	else {
-		if ((operation == "+" || operation == "/" || operation == "*" || operation == "^") && one_opertion) {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(a));
-		}
-		else if (operation == "+") {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			double b = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(b + a));
-		}
-		else  if (operation == "-" && one_opertion == false) {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			double b = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(b - a));
-		}
-		else  if (operation == "*") {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			double b = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(b * a));
-		}
-		else  if (operation == "/") {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			double b = stof(top(p_begin));
-			pop(p_begin);
-			if (a == 0) {
-				throw runtime_error("delenie na 0");
-			}
-			else {
-				push(p_begin, to_string(b / a));
-			}
-
-		}
-		else  if (operation == "^") {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			double b = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(pow(b, a)));
-		}
-		else  if (operation == "-" && one_opertion) {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(a * -1));
-		}
-		else  if (operation == "sin" && one_opertion) {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(sin(a)));
-		}
-		else  if (operation == "cos" && one_opertion) {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(cos(a)));
-		}
-		else  if (operation == "tg" && one_opertion) {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(tan(a)));
-		}
-		else  if (operation == "sqrt" && one_opertion) {
-			double a = stof(top(p_begin));
-			pop(p_begin);
-			push(p_begin, to_string(sqrt(a)));
-		}
-	}
-
-
-}
-double stack_machine(string expression) {
-	string word;
-	Node* p_top= nullptr;
-	stringstream ex_ss(expression);
-	double ot = 0;// значение по умолчанию
-	while (ex_ss >> word) {
-		//cout << word << endl;
-		if (operations.count(word) == 0 && one_operations.count(word) == 0) {
-			push(p_top, word);
+	void push(Stack*& p_top, string x) {
+		Node* n = new Node;
+		n->data = x;
+		n->next = nullptr;
+		if (p_top == nullptr) {
+			p_top = new Stack;
+			p_top->node = n;
 		}
 		else {
-			if (get_size(p_top) <= 1 || one_operations.count(word) == 1) {
-				if (get_size(p_top) == 0) {
-					throw runtime_error("Error");
+			n->next = p_top->node;
+			p_top->node = n;
+		}
+		p_top->size = p_top->size + 1;
+	}
+	string top(Stack* p_top) {
+		if (p_top == nullptr) {
+			throw "Error";
+		}
+		return p_top->node->data;
+	}
+	void pop(Stack*& p_top) {
+		if (p_top == nullptr) {
+			throw "Error";
+		}
+		else {
+			Node* t = p_top->node;
+			p_top->node = p_top->node->next;
+			delete t;
+		}
+	}
+
+	bool isEmpty(Stack*& p_top) {
+		return (p_top->node == nullptr);
+	}
+	int get_size(Stack*& p_top) {
+		return p_top->size;
+	}
+};
+struct Calculator
+{
+	Stack stack1;// обьект струкрутра стека
+	const set<string> operations = { "+","-","*","/","^","(",")" }; // операции доступные сейчас 
+	const set<string> one_operations = { "sin","cos","tg","sqrt" }; // операции доступные сейчас 
+	int get_priority(string x, int p = 0) {
+		if (x == "(" || x == ")") {
+			return 0;
+		}
+		else if (x == "+" || x == "-") {
+			return 1;
+		}
+		else if (x == "*" || x == "/") {
+			return 2;
+		}
+		else if (x == "^") {
+			return 3;
+		}
+		else if (x == "-" && p == 1) {
+			return 10;
+		}
+		else if (x == "sin" || x == "cos" || x == "tg" || x == "sqrt") {
+			return 5;
+		}
+	}
+	void operationsa_procces(Stack* p_begin, string operation, bool one_opertion = false) {
+		if ((one_opertion == false && (stack1.get_size(p_begin) <= 1))) {
+			throw runtime_error("Error");
+		}
+		else {
+			if ((operation == "+" || operation == "/" || operation == "*" || operation == "^") && one_opertion) {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(a));
+			}
+			else if (operation == "+") {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				double b = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(b + a));
+			}
+			else  if (operation == "-" && one_opertion == false) {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				double b = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(b - a));
+			}
+			else  if (operation == "*") {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				double b = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(b * a));
+			}
+			else  if (operation == "/") {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				double b = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				if (a == 0) {
+					throw runtime_error("delenie na 0");
 				}
-				//string opernd = word;// записываем унарную операцию
-				//ex_ss >> word;
-				//stack1.push(p_top, word);
-
-				operationsa_procces(p_top, word, true);
-
+				else {
+					stack1.push(p_begin, to_string(b / a));
+				}
 
 			}
-			else {
-				operationsa_procces(p_top, word);
+			else  if (operation == "^") {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				double b = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(pow(b, a)));
 			}
-
-
+			else  if (operation == "-" && one_opertion) {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(a * -1));
+			}
+			else  if (operation == "sin" && one_opertion) {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(sin(a)));
+			}
+			else  if (operation == "cos" && one_opertion) {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(cos(a)));
+			}
+			else  if (operation == "tg" && one_opertion) {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(tan(a)));
+			}
+			else  if (operation == "sqrt" && one_opertion) {
+				double a = stof(stack1.top(p_begin));
+				stack1.pop(p_begin);
+				stack1.push(p_begin, to_string(sqrt(a)));
+			}
 		}
 
+
 	}
-	ot = stof(top(p_top));
-	//для большей точности используй 
-	//long double num_ld = std::stold("123.456");
-	pop(p_top);
-	return ot;
-}
+	double stack_machine(string expression) {
+		string word;
+		Stack* p_top = stack1.create();
+		stringstream ex_ss(expression);
+		double ot = 0;// значение по умолчанию
+		while (ex_ss >> word) {
+			//cout << word << endl;
+			if (operations.count(word) == 0 && one_operations.count(word) == 0) {
+				stack1.push(p_top, word);
+			}
+			else {
+				if (stack1.get_size(p_top) <= 1 || one_operations.count(word) == 1) {
+					if (stack1.get_size(p_top) == 0) {
+						throw runtime_error("Error");
+					}
+					//string opernd = word;// записываем унарную операцию
+					//ex_ss >> word;
+					//stack1.push(p_top, word);
+
+					operationsa_procces(p_top, word, true);
+
+
+				}
+				else {
+					operationsa_procces(p_top, word);
+				}
+
+
+			}
+
+		}
+		ot = stof(stack1.top(p_top));
+		//для большей точности используй 
+		//long double num_ld = std::stold("123.456");
+		stack1.pop(p_top);
+		return ot;
+	}
+
+
+};
 int main()
 {
 	cout << "Hello user" << endl;
@@ -219,7 +225,7 @@ int main()
 			 if (stoi(x) == 1) {
 				 cout << "Input" << endl;
 				 cin >> expression;
-				 cout << stack_machine(expression) << endl;;
+				 cout << .stack_machine(expression) << endl;;
 				 //cout << sort_station << endl;
 				 //sort_station = "-1-1";
 				 //cout << sort_station << endl;
